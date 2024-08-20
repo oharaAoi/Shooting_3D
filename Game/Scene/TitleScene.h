8c0a1dd71b91e43d52321119d106bed63b70f767
oharaAoi <k023g0108@g.neec.ac.jp>
2024-08-21 01:41:19 +0900

    TitleSceneのクラスを追加

---
+++ Game/Scene/TitleScene.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class TitleScene {
+public:
+
+	TitleScene();
+	~TitleScene();
+
+	/// <summary>
+	/// 初期化関数
+	/// </summary>
+	void Init();
+
+	/// <summary>
+	/// 更新関数
+	/// </summary>
+	void Updata();
+
+	/// <summary>
+	/// 描画関数
+	/// </summary>
+	void Draw() const;
+
+private:
+
+
+};
+
